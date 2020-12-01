026c04909ecc552fb43c81c4deaf3ac398d39bb1
Naboka_EA <it_str@amaks-hotels.ru>
2020-12-01 23:47:53 +0300

    Создание дизайна главной формы Запуск формы. Ошибка

--- src/main/java/ChatGUI/ChatApp.java
+++ src/main/java/ChatGUI/ChatApp.java
@@ -6,10 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.fxml.FXMLLoader.load;
+
 public class ChatApp extends Application {
 
     public void start(Stage primaryStage) throws Exception {
-        Parent Chat= FXMLLoader.load(getClass().getResource("ChatApp.fxml"));
+        Parent Chat= FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
         primaryStage.setTitle("BlackCatChat");
         primaryStage.setScene(new Scene(Chat));
         primaryStage.show();
